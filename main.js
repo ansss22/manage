e3bd67ba61ccec06b8d6584749cbab2bd2c90f30
Anass El Amrany <elamranyanass22@gmail.com>
2025-01-30 21:36:21 +0100

    finich the styles

--- main.js
+++ main.js
@@ -4,8 +4,20 @@ const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
-    ? navToggle.setAttribute("aria-expended", false)
+    ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
+});
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false, // dots enabled 1280px and up
+    },
+  },
 });
